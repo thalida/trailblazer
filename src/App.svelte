0a8638c8e9f781b479fb7e1baf364cd2f04cb716
Thalida Noel <thalida.c.noel@gmail.com>
2020-02-17 16:27:02 -0500

    using svelte canvas repl

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,32 @@
 <script>
-	let name = 'world';
+	import { width, height } from './game.js';
+
+	import Canvas from './Canvas.svelte';
+	import Background from './Background.svelte';
+	import DotGrid from './DotGrid.svelte';
+	import Character from './Character.svelte';
+	import Text from './Text.svelte';
+	import FPS from './FPS.svelte';
 </script>
 
-<h1>Hello {name}!</h1>
+<Canvas>
+	<Background color='hsl(0, 0%, 10%)'>
+		<DotGrid divisions={30} color='hsla(0, 0%, 100%, 0.5)' />
+	</Background>
+	<Character size={10} />
+	<Text
+		text='Click and drag around the page to move the character.'
+		fontSize={12}
+		align='right'
+		baseline='bottom'
+		x={$width - 20}
+		y={$height - 20} />
+	<FPS />
+</Canvas>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
